c70f7b1aac0f8b890b1e37beddec22d0d6c569f9
cha0tica <isska@inbox.ru>
2023-08-18 22:55:34 +0300

    Экран авторизации с логотипом приложения и кнопкой «Войти». Авторизация реализована с помощью OAuth2.0.

---
+++ ImageFeed/Auth/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  ImageFeed
+//
+//  Created by Agata on 18.08.2023.
+//
+
+import Foundation
+
+let AccessKey = "0ppgNr9Pt8CmiGsnIvQDdY_tYbj8VFtaduYB4oY8ciA"
+let SecretKey = "jaJcXjFQlra1dj05SEAFgHXQmHM9DF-ta1WZ8r10YRw"
+let RedirectURI = "urn:ietf:wg:oauth:2.0:oob"
+let AccessScope = "public+read_user+write_likes"
+let DefaultBaseURL = URL(string: "https://api.unsplash.com")!
